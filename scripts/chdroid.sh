6f38ca91798f5479ea39e39ff4ab5a99eed5190e
Sylvain Ductor <ductor.sylvain@gmail.com>
2016-10-07 13:43:23 -0300

    debugage en cours

--- scripts/chdroid.sh
+++ scripts/chdroid.sh
@@ -1,82 +1,21 @@
 #!/system/bin/sh
 
                 
-#########
-#### CONFIG
-#########
-
-CHDROIDFOLDER=/sdcard/chdroid
-#DISTRIB=funtoo
-#DISTRIB=arch
-DISTRIB=debian
-
-ROOTFSDIR=$CHDROIDFOLDER/rootfs/$DISTRIB
-IMGDIR=$CHDROIDFOLDER/images
-IMG=$IMGDIR/$DISTRIB.img
-SOURCEDIR=$CHDROIDFOLDER/sources
-SOURCE=$SOURCEDIR/$DISTRIB.source
-SCRIPTDIR=$CHDROIDFOLDER/scripts
-
-POSTINSTALLSCRIPT=chdroid-pi_$DISTRIB.sh
-STARTSCRIPT=chdroid-start_$DISTRIB.sh
-STARTXSCRIPT=chdroid-startx_$DISTRIB.sh
-#FUNTOOSOURCE=???
-#ARCHSOURCE=???
-
-IMGSIZE=10G
-
-busybox=/system/xbin/busybox
-
-LOOPNO=254
-LOOPDEV=/dev/block/loop$LOOPNO
-
-SDCARD0DIR=$(readlink -f /sdcard)
-SDCARD1DIR=/storage/sdcard1 
-SDCARD0MNTDIR=media/android
-SDCARD1MNTDIR=media/ext
-
-LOGINCMD=bash -l
+source /system/xbin/chdroid-config
 
 
 
-
-########
-#### Primitives
-########
-
-function update_scripts {
-	mount -o remount,rw /system
-	cp $SCRIPTDIR/chdroid.sh	 /system/xbin/chdroid && chmod 777  /system/xbin/chdroid
-	cp $SCRIPTDIR/chdroid-util.sh	 /system/xbin/chdroid-util && chmod 777  /system/xbin/chdroid-util 
-	if [ ! -d /system/su.d ]; then
-       mkdir -m700 /system/su.d
-     fi
-    cp $SCRIPTDIR/chdroid-droidinit.sh	 /system/su.d && chmod 777  /system/su.d/chdroid-droidinit.sh
-    mount -r -o remount /system
-}
+#######
+### Mounting
+#######
 
 function mnt {
   mknod $LOOPDEV b 7 $LOOPNO
 	losetup $LOOPDEV $IMG
 	$busybox mount -t ext4 -o relatime $LOOPDEV $ROOTFSDIR
 	for f in dev dev/pts proc sys ; do $busybox mount -o bind /$f $ROOTFSDIR/$f ; done
-	if [ ! -d $ROOTFSDIR/$SDCARD0MNTDIR ]; then
-			mount -o remount,rw /system
-		  mkdir -p $ROOTFSDIR/$SDCARD0MNTDIR
-	fi
-	if [ ! -d $ROOTFSDIR/$SDCARD1MNTDIR ]; then
-			mount -o remount,rw /system
-		  mkdir -p $ROOTFSDIR/$SDCARD1MNTDIR
-	fi
 	$busybox mount -o bind $SDCARD0DIR $ROOTFSDIR/$SDCARD0MNTDIR
 	$busybox mount -o bind $SDCARD1DIR $ROOTFSDIR/$SDCARD1MNTDIR
-}
-
-function log_in {
-	env -i  USER=root HOME=/root TERM=$TERM PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $busybox chroot $ROOTFSDIR $LOGINCMD
-}
-function startx {
-	env -i  USER=root HOME=/root TERM=$TERM PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $busybox chroot $ROOTFSDIR  bash -l -c $STARTXSCRIPT
 }
 
 
@@ -86,6 +25,26 @@ function unmnt {
 	losetup -d $LOOPDEV
 	rm $LOOPDEV
 }
+
+
+
+#######
+### Starting
+#######
+
+
+
+function log_in {
+	env -i  USER=root HOME=/root TERM=$TERM PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $busybox chroot $ROOTFSDIR bash -l 
+}
+function startx {
+	env -i  USER=root HOME=/root TERM=$TERM PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $busybox chroot $ROOTFSDIR  bash -l -c $STARTXSCRIPT
+}
+
+
+#######
+### Installation
+#######
 
 function mkimg_tar {
 		#wget -c $REMOTESOURCE
@@ -99,6 +58,11 @@ function mkimg_tar {
 
  
 function install {
+  echo "creating env directories"
+	mount -o remount,rw /system
+  mkdir -p $ROOTFSDIR/$SDCARD0MNTDIR
+  mkdir -p $ROOTFSDIR/$SDCARD1MNTDIR
+  mount -r -o remount /system
 	#if arch funtoo then mkimg_tar
 	echo "copying the source image as new rootfs img"
 	cp $SOURCE $IMG
@@ -117,74 +81,63 @@ function install {
 	env -i  USER=root HOME=/root TERM=$TERM PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $busybox chroot $ROOTFSDIR bash -l -c $POSTINSTALLSCRIPT
 }
  
+
+#######
+### SubMain
+#######
+
+function interactive_menu {
+  OPTIONS="Mount Login Startx Umount Install Update Exit"
+	select opt in $OPTIONS; do
+		 if [ "$opt" = "Mount" ]; then
+			mnt
+		 elif [ "$opt" = "Login" ]; then
+			log_in
+			exit
+		 elif [ "$opt" = "Startx" ]; then
+			startx
+			exit
+		 elif [ "$opt" = "Install" ]; then
+			install
+			exit
+		 elif [ "$opt" = "Umount" ]; then
+			umnt
+		 elif [ "$opt" = "Update" ]; then
+			update_scripts 
+			exit
+		 elif [ "$opt" = "Exit" ]; then
+			echo Exiting
+			exit
+		 else
+			clear
+			echo bad option
+		 fi
+	done
+}
+
+function usage {
+
+}
+
 #######
 ### Main
 #######
 
 echo "This is chroot for Android"
 
-while [[ $# -gt 1 ]]
-do
-key="$1"
-
-case $key in
-    -t|--root-folder)
-		CHDROIDFOLDER="$2"
-    shift # past argument
-    ;;
-    -d|--distrib)
-    DISTRIB="$2"
-    shift # past argument
-    shift # past argument or value
-    ;;
-    -s|--size)
-    IMGSIZE="$2"
-    shift # past argument
-    shift # past argument or value
-    ;;
-    mount)
-    echo "mounting..."
-    mnt
-		break
-    ;;
-    login)
-    log_in
-    exit
-    ;;
-    startx)
-    startx
-    exit
-    ;;
-    *)
-    # unknown option
-    ;;
+cmd="$1"
+case $cmd in
+  i|interactive) interactive_menu                          ;;
+  m|mount)     echo "mounting..."   	    ; mnt		       	 ;;
+  u|umount)    echo "unmounting..." 	    ; umnt 	      	 ;;
+  l|login)     echo "login..."      	    ; log_in       	 ;;
+  x|startx)    echo "starting x..."       ; startx         ;;
+  in|install)  echo "installing..."       ; install        ;;
+  up|update)   echo "updating scripts..." ; update_scripts ;;
+  *)           echo "unknown option"      ; usage          ;;
 esac
-done
 
 
 
-OPTIONS="Mount Login Umount Install Update Exit"
-
-select opt in $OPTIONS; do
-   if [ "$opt" = "Mount" ]; then
-    mnt
-   elif [ "$opt" = "Login" ]; then
-    log_in
-		exit
-   elif [ "$opt" = "Install" ]; then
-    install
-		exit
-   elif [ "$opt" = "Umount" ]; then
-    umnt
-   elif [ "$opt" = "Update" ]; then
-    update_scripts 
-   elif [ "$opt" = "Exit" ]; then
-    echo Exiting
-    exit
-   else
-    clear
-    echo bad option
-   fi
-done
 
 exit 0
